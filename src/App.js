0aea829639e2c14371ad5ae48c26c4140333f9f6
alphacpc <alphacpc@gmail.com>
2021-09-06 16:27:06 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Box1">
+
+
+      
+      </div>
+      <div className="Box2">
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
