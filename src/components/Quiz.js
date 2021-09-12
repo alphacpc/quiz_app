15ee0aa6e1f9bb6b112fa14f5bb0eed9a43bc17b
alphacpc <alphacpc@gmail.com>
2021-09-12 15:06:24 +0000

    Static question and Design Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProgressBar from './ProgressBar';
+import {FaUserGraduate} from 'react-icons/fa';
 
 
 const Quiz = () => {
@@ -8,10 +9,26 @@ const Quiz = () => {
             <ProgressBar />            
             
             <div className="mainQuiz">
+                <div className="divLevel">
+                    <span className="titleLevel"> <FaUserGraduate/> Niveau:</span>
+                    <span className="valueLevel">Debutant</span>
+                </div>
+                
+                <div className="divQuiz">
+                    <h2 className="Question">Quelle est la capital du Senegal ?</h2>
+                    <div className="divAnswers">
+                        <p className="Answer">Conakry</p>
+                        <p className="Answer">Lagos</p>
+                        <p className="Answer selected">Dakar</p>
+                        <p className="Answer">Addis-Abeba</p>
+                    </div>
+
+                    <button className="btnValidAnswer" type="submit">Valider</button>
+                </div>
+        
             </div>
         </React.Fragment>
-        
     )
 }
 
-export default Quiz
+export default Quiz;
