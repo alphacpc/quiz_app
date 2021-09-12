0e5a8f005548047c226dc40c338cc541f243cf88
alphacpc <alphacpc@gmail.com>
2021-09-12 13:06:06 +0000

    Ajout des questions quiz

---
+++ src/Questions.js
@@ -0,0 +1,189 @@
+export const Quiz = [
+    {   
+        sujet: "Un projet d'application quiz (des questions relatives à l'afrique)",
+        quiz:{
+            debutant:[
+               
+                {
+                    id: 1,
+                    question:"Quelle est la religion dominante en Afrique ?",
+                    options:["L'Islam","La religion catholique","Les religions tribales"],
+                    answer:""
+                },
+                {
+                    id: 2,
+                    question:"Quel est le sommet le plus haut d’Afrique ?",
+                    options:["Le Mawenzi","Le Kilimandjaro","le Mont Kenya"],
+                    answer:"Le Kilimandjaro"
+                },
+                {
+                    id: 3,
+                    question:"Le Cap-Vert, qui se situe à 644 kilomètres des côtes du Sénégal, a pour capitale",
+                    options:["Sal","Boa Vista","Praia","Santiago"],
+                    answer:""
+                },
+                {
+                    id: 4,
+                    question:" L’Afrique est le berceau de l’humanité puisque les premiers hommes y vivaient déjà il y a 7 millions d’années.",
+                    options:["Vrai","Faux"],
+                    answer:"Vrai"
+                },
+                {
+                    id: 5,
+                    question:"Qui est Sékou Touré ?",
+                    options:["Un footballeur béninois","Le premier président de la Guinée","Un article malien","Un compagnon de route de Nelson Mandela"],
+                    answer:"Le premier président de la Guinée"
+                },
+                {
+                    id: 6,
+                    question:"Combien de pays d'Afrique n'ont jamais été colonisés ?",
+                    options:[0, 1, 1.5 ,2],
+                    answer:""
+                },
+                {
+                    id: 7,
+                    question:"Qu'est ce que le panafricanisme ?",
+                    options:[
+                        "C'est une religion pratiquée essentiellement en Afrique australe",
+                        "C'est une langue, sorte d'esperanto qui ne mélange que des langues africaines",
+                        "C'est une méthode de cuisine qui saisit les aliments dans une poêle (pan en anglais) en argile posée sur un feu puissant",
+                        "C'est une doctrine prônée par Sékou Touré"
+                    ],
+                    answer:"Vrai"
+                },
+                {
+                    id: 8,
+                    question:"Qu'est ce que la négritude ?",
+                    options:[
+                        "Un chant de libération du Niger",
+                        "Un mouvement littéraire et politique",
+                        "Une coiffure de femme à la mode sous Napoléon",
+                        "Un style de danse"
+                    ],
+                    answer:"Un mouvement littéraire et politique"
+                },
+                {
+                    id: 9,
+                    question:"Quel est l'ancien nom du Mali ?",
+                    options:["Le Dahomey","La Haute-volta","L'Ile de France", "Le Soudan français"],
+                    answer:"Le Soudan français"
+                },
+                {
+                    id: 10,
+                    question:"Quel est ce pays africain, avec une population plus de 200 millions d'habitants, principalement producteur de pétrole ?",
+                    options:["Le Niger","Le Nigéria","L'Angola","La RDC"],
+                    answer:"Le Nigéria"
+                },
+
+
+                {
+                    id: 11,
+                    question:"Combien de pays africains ont des drapeaux vert-jaune-rouge (avec 0-1-2 étoile(s)) ?",
+                    options:[5, 7, 11, 17],
+                    answer:""
+                },
+                {
+                    id: 12,
+                    question:"Quelle est la capitale du Maroc ?",
+                    options:["Dagana", "Alger","Accra","Rabat"],
+                    answer:"Rabat"
+                },
+                {
+                    id: 13,
+                    question:"Dans quel océan se jette le fleuve Zambèze ?",
+                    options:["Océan Indien","Océan Atlantique","Océan Pacifique","Océan Antarctique"],
+                    answer:"Océan Indien"
+                },
+                {
+                    id: 14,
+                    question:"Quelle est cette péninsule de l'Afrique de l'Est qui s'étend depuis la côte sud de la mer Rouge jusqu'à la côte ouest de la mer d'Arabie ?",
+                    options:["Détroit de l'Afrique", "Corne de l'Afrique", "Le Kilimandjaro","Détroit de Gilbraltar"],
+                    answer:""
+                },
+                {
+                    id: 15,
+                    question:"Combien y a-t-il de pays en Afrique ?",
+                    options:[54, 68, 53, 55],
+                    answer: 54
+                },
+                {
+                    id: 16,
+                    question:"Quel est le classement du continent africain par rapport aux autres continents, en terme de population ?",
+                    options:["1er","2e","3e","4e"],
+                    answer:"2e"
+                },
+
+
+
+                {
+                    id: 16,
+                    question:"Combien y a-t-il de langues vivantes sur le continent africain ?",
+                    options:["Environ 500","Environ 1000","Environ 2000","Environ 4000"],
+                    answer:"Environ 2000"
+                },
+                {
+                    id: 16,
+                    question:"Dans quels pays se situent les chutes Victoria ?",
+                    options:["Kenya et Tanzanie","Mozambique et Malawi","Zambie et Zimbabwe"],
+                    answer:""
+                },
+                {
+                    id: 16,
+                    question:"Quelle est la monnaie du Cameroun ?",
+                    options:["L'euro","Le franc CFA","Le dollar camerounais","Le franc camerounais"],
+                    answer:""
+                },
+                {
+                    id: 16,
+                    question:"Quelle est la capitale de l’Éthiopie ?",
+                    options:["Nairobi","Addis-Adeba","Kigali","Awasa"],
+                    answer:"Addis-Adeba"
+                },
+                {
+                    id: 16,
+                    question:"Quel est le bon classement des 4 pays les plus riches en afrique (de l'année 2021) ?",
+                    options:[
+                        "Nigeria, Afrique du Sud, Egypte",
+                        "Afrique du Sud, Nigeria, Egypte",
+                        "Afrique du Sud, Egypte, Algerie",
+                        "Nigeria, Angola, Ethiopie"
+                    ],
+                    answer: "Nigeria, Afrique du Sud, Egypte"
+                },
+                {
+                    id: 16,
+                    question:"Quel est le vrai nom de l'Afrique",
+                    options:["Afrique","Africa","Alkebulan","Terre afrique"],
+                    answer:"Alkebulan"
+                },
+
+
+
+
+
+
+
+                //Quiz Premier
+
+                {
+                    id: 16,
+                    question:"Le premier africain a intégré la NASA",
+                    options:["Cheick Modibo Diarra","Cheikh Anta Diop","Nelson Mandela","George Weah"],
+                    answer:"2e"
+                },
+                {
+                    id: 16,
+                    question:"",
+                    options:["1er","2e","3e","4e"],
+                    answer:"2e"
+                },
+                {
+                    id: 16,
+                    question:"",
+                    options:["1er","2e","3e","4e"],
+                    answer:"2e"
+                },
+            ]
+        }
+    }
+]
